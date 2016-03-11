f6aa8e14d846903f3011617b52eb2f7628f282ff
Slava Fomin II <s.fomin@betsol.ru>
2016-03-11 18:43:26 +0300

    Added script for Node.js

---
+++ scripts/install-nodejs.sh
@@ -0,0 +1,16 @@
+#/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+NODE_VERSION="v5"
+
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+. $HOME/.nvm/nvm.sh
+
+cd "$NVM_DIR" && git pull origin master && git checkout `git describe --abbrev=0 --tags`
+. $HOME/.nvm/nvm.sh
+
+nvm install $NODE_VERSION
+nvm use $NODE_VERSION
+nvm alias default $NODE_VERSION
